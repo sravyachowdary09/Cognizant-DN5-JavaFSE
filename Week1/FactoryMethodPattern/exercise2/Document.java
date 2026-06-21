e23eafefadd45e3f56175179b4d9bd25bb457253
Kilaru Sravya <2300030786@kluniversity.in>
2026-06-21 21:51:50 +0530

    Add files via upload
    Week 1 Design Patterns Solutions

---
+++ Week1/FactoryMethodPattern/exercise2/Document.java
@@ -0,0 +1,5 @@
+package exercise2;
+
+public interface Document {
+    void open();
+}
---
+++ Week1/FactoryMethodPattern/exercise2/PdfDocumentFactory.java
@@ -0,0 +1,8 @@
+package exercise2;
+
+public class PdfDocumentFactory extends DocumentFactory {
+
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
